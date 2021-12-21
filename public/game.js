79af0a47b0508787a245c09b1c78f64eb8e0aa08
f4bioreis <fabiopinto123@hotmail.com>
2021-12-21 08:43:36 -0300

    actual game modules

---
+++ public/game.js
@@ -0,0 +1,111 @@
+export default function createGame() {
+    console.log('createGame: init');
+    const state = {
+        players: {},
+        fruits: {},
+        screen: {
+            width: 10,
+            height: 10
+        }
+    };
+    const currentPlayerId = 'player1';
+
+    function addPlayer(command) {
+        const playerId = command.playerId;
+        const playerX = command.playerX;
+        const playerY = command.playerY;
+        const playerWidth = command.width;
+        const playerHeight = command.height;
+        const playerColor = command.color;
+
+        state.players[playerId] = {
+            x: playerX,
+            y: playerY,
+            width: playerWidth,
+            height: playerHeight,
+            color: playerColor
+        };
+    }
+
+    function removePlayer(command) {
+        const playerId = command.playerId;
+        delete state.players[playerId];
+    }
+
+    function addFruit(command) {
+        const fruitId = command.fruitId;
+        const fruitX = command.fruitX;
+        const fruitY = command.fruitY;
+        const fruitWidth = command.width;
+        const fruitHeight = command.height;
+        const fruitColor = command.color;
+
+        state.fruits[fruitId] = {
+            x: fruitX,
+            y: fruitY,
+            width: fruitWidth,
+            height: fruitHeight,
+            color: fruitColor
+        };
+    }
+
+    function removeFruit(command) {
+        const fruitId = command.fruitId;
+        delete state.fruits[fruitId];
+    }
+    
+    function movePlayer(command) {
+        console.log(`Moving player ${command.playerId} with command ${command.keyPressed}`);
+
+        const acceptedMoves = {
+            ArrowUp(player) {
+                player.y--;
+                console.log(state.players[currentPlayerId].x, state.players[currentPlayerId].y);
+            },
+            ArrowDown(player) {
+                player.y++;
+                console.log(state.players[currentPlayerId].x, state.players[currentPlayerId].y);
+            },
+            ArrowRight(player) {        
+                player.x++;
+                console.log(state.players[currentPlayerId].x, state.players[currentPlayerId].y);
+            },
+            ArrowLeft(player) {
+                player.x--;
+                console.log(state.players[currentPlayerId].x, state.players[currentPlayerId].y);
+            }
+        }
+
+        const player = state.players[command.playerId];
+        const playerId = command.playerId;
+        const keyPressed = command.keyPressed;
+        const moveFunction = acceptedMoves[keyPressed];
+
+        if (moveFunction) {
+            moveFunction(player);
+            checkForFruitCollision(playerId);
+        }
+
+        function checkForFruitCollision(playerId) { // Brute force
+            let fruit;
+            const player = state.players[playerId];
+            for (const fruitId in state.fruits) {
+                fruit = state.fruits[fruitId];
+                if (player.x === fruit.x && player.y === fruit.y) {
+                    console.log(`Collision between player ${playerId} and fruit ${fruitId}`);
+                    removeFruit({fruitId})
+                }
+            }
+        }
+        
+    }
+
+    return {
+        addPlayer,
+        removePlayer,
+        movePlayer,
+        addFruit,
+        removeFruit,
+        state
+    };
+}
---
+++ public/keyboard-listener.js
@@ -0,0 +1,31 @@
+export default function createKeyboardListener(document) {
+    const state = {
+        observers: []
+    };
+    document.addEventListener('keydown', handleKeydown);
+    return { subscribe };
+
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction);
+    }
+
+    function notifyAll(command) {
+        console.log('Notifying all ' + state.observers.length + ' observer(s)');
+
+        for (const observerFunction of state.observers) {
+            observerFunction(command);
+        }
+    }
+
+    function handleKeydown(event) {
+        const keyPressed = event.key;
+
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        };
+
+        notifyAll(command);
+    }
+
+}
